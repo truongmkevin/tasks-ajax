166676a27afcacfc9a7172598bb9277c47d4ac95
truongmkevin <truongmkevin@yahoo.com>
2015-06-11 09:41:02 -0500

    Added bootstrap theme, tested with one user task.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :user_tasks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'user_tasks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
